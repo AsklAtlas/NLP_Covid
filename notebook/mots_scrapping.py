2a1d84a4cc2231b3d81ad2d3518e4c51a0d7272e
Danpdvn <danielpdvn@gmail.com>
2021-04-08 21:25:09 +0200

    First commit

---
+++ notebook/mots_scrapping.py
@@ -0,0 +1,10 @@
+key_words = {
+"étudiants" : ["étudiants","université","enseignement","cours","études","cursus","stage","éducation","logement","étudier"],
+"dépression" : ["dépression","dépressif","tristesse","fatigue","décourager","crise","stress","pression","insomnie","chômage","souffre","souffrir","anxiété","anxieux"],
+"isolement" : ["isolement","solitude","abandonné","mental","pensée","abandon"],
+"suicide" : ["suicide","décès","mort","suicidaires"],
+"psy" : ["consulter","psy","psychologique"],
+"confinement" : ["confinement", "coronavirus"],
+"coronavirus" : ["coronavirus","confinement","Covid","covid-19","virus","pandémie","épidémie","clusters"]
+}
+
